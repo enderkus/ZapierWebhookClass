b330c124620587c574caa252de48c27e0dc60933
Ender KUŞ <enderkus@msn.com>
2022-11-21 21:12:20 +0300

    Create example.php

---
+++ example.php
@@ -0,0 +1,14 @@
+<?php 
+require_once('webhookclass.php);
+
+// Calling the class.
+$zwh = new ZapierWebhook();
+
+// We define the webhook URL.
+$zwh->hookUrl = "https://hooks.zapier.com/hooks/catch/xxxxxxxxxx/xxxxxx/";
+
+// Using for plain text
+$zh->webhookInitPlainText('Hello Zapier !');
+
+// Using for plain JSON
+$zh->webhookInit(array('first_name'=>'Johny','last_name'=>'Bravo'));
